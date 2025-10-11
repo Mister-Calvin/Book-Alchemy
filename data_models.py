4f4260c46e7954a7ed41589945891c4bf4c394b7
Mister-Calvin <calvin@kuven.de>
2025-10-11 16:46:57 +0200

    Add unique constraint to ISBN field in Book model to enforce data integrity

--- data_models.py
+++ data_models.py
@@ -1,3 +1,5 @@
+from enum import unique
+
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -22,7 +24,7 @@ class Author(db.Model):
 class Book(db.Model):
     __tablename__ = 'books'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    isbn = db.Column(db.String, nullable=False)
+    isbn = db.Column(db.String,unique=True ,nullable=False)
     title = db.Column(db.String, nullable=False)
     publication_year = db.Column(db.Date, nullable=False)
 
